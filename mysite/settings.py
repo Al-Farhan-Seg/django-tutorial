192d084312779b42b571302cbb7161f13b0bbb68
Al-Farhan-Seg <farhan.segujja@gmail.com>
2026-09-25 10:49:39 +0300

    Done with 'Writing your first Django app, part2'

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EAT'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("al-farhan", views.al_farhan, name="al-farhan")
+    path("", views.index, name="index"),    
 ]
