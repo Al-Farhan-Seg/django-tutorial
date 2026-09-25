9f395eba02a7d6fe437cb3d9ce478d48616ae127
Al-Farhan-Seg <farhan.segujja@gmail.com>
2026-09-25 10:03:27 +0300

    Folder restructuring

--- polls/views.py
+++ polls/views.py
@@ -4,5 +4,3 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def al_farhan(request):
-    return HttpResponse("Farhan has tried it out very well!!!")
